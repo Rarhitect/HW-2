760e176071ff32dca2cb0af7604de82113247702
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-16 22:06:06 +0300

    Capacity увеличилась вдвое!

--- HW2-1/main.cpp
+++ HW2-1/main.cpp
@@ -1,11 +1,20 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    
+    vector<int> v(3);
+    cout << "size = " << v.size() << "; capacity = " << v.capacity() << endl;
+    for(auto i: v)
+    {
+        v[i] = 1;
+        cout << v[i] << endl;
+    }
+    v.push_back(1);
+    cout << "size = " << v.size() << "; capacity = " << v.capacity() << endl;
     return 0;
 }
