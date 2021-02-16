efeacac24acc118974a8e3b4eb6e2721eddb8303
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-16 22:20:13 +0300

    При резервировании емкости, переполнение даст нам еще N зарезервированных ячеек

--- HW2-1/main.cpp
+++ HW2-1/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main()
 {
+    //Часть 1
     vector<int> v(3);
     cout << "size = " << v.size() << "; capacity = " << v.capacity() << endl;
     for(auto i: v)
@@ -16,5 +17,19 @@ int main()
     }
     v.push_back(1);
     cout << "size = " << v.size() << "; capacity = " << v.capacity() << endl;
+    
+    //Часть 2
+    vector<int> v1(3);
+    v1.reserve(5);
+    cout << "size = " << v1.size() << "; capacity = " << v1.capacity() << endl;
+    for(auto i: v1)
+    {
+        v1[i] = 1;
+        cout << v1[i] << endl;
+    }
+    v1.push_back(1);
+    v1.push_back(1);
+    v1.push_back(1);
+    cout << "size = " << v1.size() << "; capacity = " << v1.capacity() << endl;
     return 0;
 }
