b6194c239af0fe2aa941b3a6d867baed097c96a6
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-17 20:13:01 +0300

    Добавил родную сортировку для контейнеров - программа выводит результаты нашего "турнира"

--- HW2-2/main.cpp
+++ HW2-2/main.cpp
@@ -11,14 +11,24 @@
 using namespace std;
 
 template<typename T>
-void sort_time(T object)
+void std_sort_time(T object)
 {
     auto begin = std::chrono::steady_clock::now();
     std::sort(object.begin(), object.end());
     auto end = std::chrono::steady_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-    std::cout << "Time: " << elapsed.count() << " ns" << std::endl;
+    std::cout << "std::sort time: " << elapsed.count() << " ns" << std::endl;
 };
+
+template<typename T>
+void native_sort_time(T object)
+{
+    auto begin = std::chrono::steady_clock::now();
+    object.sort();
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    std::cout << "native sort time: " << elapsed.count() << " ns" << std::endl;
+}
 
 int main()
 {
@@ -29,19 +39,19 @@ int main()
     deque<int> d = {3, 4, 1, 2, 7, 21, 33, 17, 56, 35};
     
     cout << "Vector - ";
-    sort_time(v);
+    std_sort_time(v);
     
     cout << "Array - ";
-    sort_time(a);
+    std_sort_time(a);
     
-//    cout << "List - ";
-//    sort_time(l);
-//    
-//    cout << "Forward list - ";
-//    sort_time(fl);
+    cout << "List - ";
+    native_sort_time(l);
+    
+    cout << "Forward list - ";
+    native_sort_time(fl);
     
     cout << "Deque - ";
-    sort_time(d);
+    std_sort_time(d);
     
     return 0;
 }
