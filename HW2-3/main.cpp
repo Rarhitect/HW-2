78423e2e4c55c49cad9d85c6e0855790448862fe
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-18 14:15:19 +0300

    Initial Commit

---
+++ HW2-3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW2-3
+//
+//  Created by Alyaev Roman on 18.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
